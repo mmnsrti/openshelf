a7e2e6c19dd489891f483963733f82bef72f699b
mmnsrti <mmnsrti@gmail.com>
2025-02-08 13:36:53 +0330

    `Update ColorPicker component to use provided props instead of local state`

--- components/admin/ColorPicker.tsx
+++ components/admin/ColorPicker.tsx
@@ -1,17 +1,21 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import { HexColorInput, HexColorPicker } from "react-colorful";
 interface Props {
   value?: string;
   onPickerChange: (color: string) => void;
 }
 const ColorPicker = ({ value, onPickerChange }: Props) => {
-  const [color, setColor] = useState("#aabbcc");
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <HexColorPicker color={color} onChange={setColor} />
-      <HexColorInput color={color} onChange={setColor} className="hex-input" />
+      <HexColorPicker color={value} onChange={onPickerChange} />
+      <div className="flex flex-row items-center">
+        <p>#</p>
+        <HexColorInput
+          color={value}
+          onChange={onPickerChange}
+          className="hex-input"
+        />
+      </div>
     </div>
   );
 };
