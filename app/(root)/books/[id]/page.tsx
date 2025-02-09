e263f02696227c726e04bf77ae87d12bb98ea405
mmnsrti <mmnsrti@gmail.com>
2025-02-09 13:39:54 +0330

    `feat: updated page component for book details with authentication and database query`

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,8 +1,44 @@
+import { auth } from "@/auth";
+import BookOverview from "@/components/BookOverview";
+import BookVideo from "@/components/BookVideo";
+import { db } from "@/database/drizzel";
+import { books } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = ({ params }: { params: string }) => {
-  console.log(params.id);
-  return <div>{} dd</div>;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const session = await auth();
+  const id = (await params).id;
+  const [bookDetails] = await db
+    .select()
+    .from(books)
+    .where(eq(books.id, id))
+    .limit(1);
+  if (!bookDetails) {
+    return redirect("/404");
+  }
+  return (
+    <>
+      <BookOverview {...bookDetails} userId={session?.user?.id as string} />
+      <div className="book-details">
+        <div className="flex-[1.5]">
+          <section className="flex flex-col gap-7">
+            <h3>video</h3>
+            <BookVideo videoUrl={bookDetails.videoUrl} />
+          </section>
+          <section className="flex flex-col gap-7">
+            <h3>Summary</h3>
+            <div className="space-y-5 text-xl text-light-100">
+              {bookDetails.summary.split("/n").map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default page;
