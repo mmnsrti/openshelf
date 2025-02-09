cb276938f8a0a51ee316385769d06fcde03629c6
mmnsrti <mmnsrti@gmail.com>
2025-02-09 07:44:46 +0330

    `Added authentication checks and admin role verification to admin layout`

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,15 +4,26 @@ import React from "react";
 import "@/styles/admin.css";
 import SideBar from "@/components/admin/SideBar";
 import Header from "@/components/admin/Header";
+import { db } from "@/database/drizzel";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
-  //   if (!session?.user?.id) {
-  //     return redirect("/sign-in");
-  //   }
+  if (!session?.user?.id) {
+    return redirect("/sign-in");
+  }
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+  if (!isAdmin) {
+    return redirect("/");
+  }
   return (
     <main className="flex min-h-screen w-full flex-row">
       <SideBar session={session} />
-
       <div className="admin-container">
         <Header session={session} />
         {children}
